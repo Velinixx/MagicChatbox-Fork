8f81b4599153233e903389819c59cfc6406bc428
Velinixx <velinixx@users.noreply.github.com>
2026-08-27 11:53:14 -0500

    C20 BLE: add missing using System.Threading

--- vrcosc-magicchatbox/Classes/Modules/C20BleClient.cs
+++ vrcosc-magicchatbox/Classes/Modules/C20BleClient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using vrcosc_magicchatbox.Classes.DataAndSecurity;
 using Windows.Devices.Bluetooth;
